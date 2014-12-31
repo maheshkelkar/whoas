8087920b717ad89879fd10bae120277a47bb2c31
R. Tyler Croy <tyler@monkeypox.org>
2014-12-30 21:23:12 -0800

    Add support for supplying your own Queue implementation to the InMemoryQueue
    This will make it a bit easier to test inside of Spock with pre-configured/set
    up queues

--- src/main/groovy/com/github/lookout/whoas/InMemoryQueue.groovy
+++ src/main/groovy/com/github/lookout/whoas/InMemoryQueue.groovy
@@ -1,16 +1,26 @@
 package com.github.lookout.whoas
 
-
+import java.util.Queue
 import java.util.concurrent.LinkedBlockingQueue
 
 /**
  * A simple in-memory queue that offers no persistence between process restarts
  */
 class InMemoryQueue implements IHookQueue {
-    private LinkedBlockingQueue internalQueue
+    private Queue internalQueue
 
+    /**
+     * Create the InMemoryQueue with it's own internal queueing implementation
+     */
     InMemoryQueue() {
         this.internalQueue = new LinkedBlockingQueue()
+    }
+
+    /**
+     * Create the InMemoryQueue with the given Queue object
+     */
+    InMemoryQueue(Queue queue) {
+        this.internalQueue = queue
     }
 
     Long getSize() {
--- src/test/groovy/com/github/lookout/whoas/InMemoryQueueSpec.groovy
+++ src/test/groovy/com/github/lookout/whoas/InMemoryQueueSpec.groovy
@@ -1,5 +1,6 @@
 package com.github.lookout.whoas
 
+import java.util.concurrent.ArrayBlockingQueue
 import spock.lang.*
 
 class InMemoryQueueSpec extends Specification {
@@ -7,6 +8,18 @@ class InMemoryQueueSpec extends Specification {
     def "getSize() should return 0 by default"() {
         given:
         InMemoryQueue queue = new InMemoryQueue()
+
+        expect:
+        queue.size == 0
+    }
+}
+
+
+class InMemoryQueueWithGivenQueueSpec extends Specification {
+    def "getSize() should return 0 by default"() {
+        given:
+        ArrayBlockingQueue internal = new ArrayBlockingQueue(1)
+        InMemoryQueue queue = new InMemoryQueue(internal)
 
         expect:
         queue.size == 0
