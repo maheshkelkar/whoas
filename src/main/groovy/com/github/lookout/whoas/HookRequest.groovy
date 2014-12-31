2689feb2cdfb8ad55e9d568865f54ec59812cff1
R. Tyler Croy <tyler@monkeypox.org>
2014-12-30 21:22:52 -0800

    Add a simple default constructor for whoas library consumers to use

--- src/main/groovy/com/github/lookout/whoas/HookRequest.groovy
+++ src/main/groovy/com/github/lookout/whoas/HookRequest.groovy
@@ -13,6 +13,16 @@ class HookRequest {
     /* Constructor for Jackson */
     HookRequest() { }
 
+    /**
+     * Default constructor for creating a simple HookRequest with a URL and the
+     * POST data to be delivered to that URL
+     */
+    HookRequest(String hookUrl, String hookData) {
+        this.retries = 0
+        this.url = hookUrl
+        this.postData = hookData
+    }
+
     @JsonProperty
     Long getRetries() {
         return this.retries
